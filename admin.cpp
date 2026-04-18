a309b96a9582e616fa8fd296414f70dfdb5c282a
Ramë Hadergjonaj <rame.hadergjonaj@student.uni-pr.edu>
2026-04-18 17:44:04 +0200

    Implement admin delete command with system file protection

--- admin.cpp
+++ admin.cpp
@@ -1,2 +1,21 @@
 #include "server_funksionet.h"
 #include <sys/stat.h>
+
+void fshijFile(char* emri, char* rezultati) {
+    while (emri[0] == ' ') {
+        for (int i = 0; i < (int)strlen(emri); i++) {
+            emri[i] = emri[i+1];
+        }
+    }
+    
+    if (strcmp(emri, "server.exe") == 0 || strcmp(emri, "klient.exe") == 0) {
+        sprintf(rezultati, "GABIM: Nuk mund te fshihet file-i i sistemit '%s'!\n", emri);
+        return;
+    }
+    
+    if (remove(emri) == 0) {
+        sprintf(rezultati, "SUKSES: File-i '%s' u fshi!\n", emri);
+    } else {
+        sprintf(rezultati, "GABIM: Nuk mund te fshihet '%s'\n", emri);
+    }
+}
