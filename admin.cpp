de7efe230a1857fdb405cb169e16edb3515d2cd1
Ramë Hadergjonaj <rame.hadergjonaj@student.uni-pr.edu>
2026-04-18 17:45:53 +0200

    Add admin search command for matching file names

--- admin.cpp
+++ admin.cpp
@@ -19,3 +19,32 @@ void fshijFile(char* emri, char* rezultati) {
         sprintf(rezultati, "GABIM: Nuk mund te fshihet '%s'\n", emri);
     }
 }
+
+void kerkoFile(char* fjala, char* rezultati) {
+    while (fjala[0] == ' ') {
+        for (int i = 0; i < (int)strlen(fjala); i++) {
+            fjala[i] = fjala[i+1];
+        }
+    }
+    
+    sprintf(rezultati, "========== REZULTATET E KERKIMIT ('%s') ==========\n", fjala);
+    int gjetur = 0;
+    
+    system("dir /b > temp_kerko.txt");
+    ifstream file("temp_kerko.txt");
+    char rreshti[200];
+    while (file.getline(rreshti, 200)) {
+        if (strstr(rreshti, fjala) != NULL) {
+            strcat(rezultati, "  ");
+            strcat(rezultati, rreshti);
+            strcat(rezultati, "\n");
+            gjetur = 1;
+        }
+    }
+    file.close();
+    system("del temp_kerko.txt");
+    
+    if (gjetur == 0) {
+        strcat(rezultati, "  Nuk u gjet asnje file.\n");
+    }
+}
